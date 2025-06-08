2e45ec3b421946c6facc855b265b94c867ca9213
HappyLappy1 <86489014+HappyLappy1@users.noreply.github.com>
2025-06-07 23:57:03 -0400

    Huh. That shouldn't be missing

--- Special Processes/SetDemoCharactersToTeam.asm
+++ Special Processes/SetDemoCharactersToTeam.asm
@@ -26,6 +26,7 @@
 ;.definelabel ProcStartAddress, 0x022E7B88
 ;.definelabel ProcJumpAddress, 0x022E8400
 ;.definelabel AssemblyPointer, 0x20B138C
+;.definelabel SpeciesIDToStore, 0x20B0818
 
 ; File creation
 .create "./code_out.bin", 0x022E7248 ; For EU: 0x022E7B88
